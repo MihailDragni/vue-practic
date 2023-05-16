d75d57497dafec54b882d2540b3b126effd23e66
MihailDragni <74327586+MihailDragni@users.noreply.github.com>
2023-05-17 00:26:05 +0300

    added some components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,71 @@
 <template>
-  <div>Работает</div>
+  <div class="app">
+    <h1>Страница с постами</h1>
+    <my-button @click="showDialog">Создать пост</my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost" />
+    </my-dialog>
+
+    <posts-list :posts="posts" @remove="removePost" />
+  </div>
 </template>
 
 <script>
+import PostForm from '@/components/PostForm.vue'
+import PostsList from '@/components/PostsList.vue'
 export default {
+  components: { PostForm, PostsList },
   name: 'App',
   data() {
     return {
-      likes: 0,
+      posts: [
+        {
+          id: 1,
+          title: 'JavaScript 1',
+          body: 'Описание поста 1 ',
+        },
+        {
+          id: 2,
+          title: 'JavaScript 2',
+          body: 'Описание поста 2',
+        },
+        {
+          id: 3,
+          title: 'JavaScript 3',
+          body: 'Описание поста 3',
+        },
+        {
+          id: 4,
+          title: 'JavaScript 4',
+          body: 'Описание поста 4',
+        },
+      ],
+      dialogVisible: false,
     }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post)
+      this.dialogVisible = false
+    },
+    removePost(post) {
+      this.posts = this.posts.filter((p) => p.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true
+    },
   },
 }
 </script>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  padding: 20px;
+}
+</style>
---
+++ src/components/PostsList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="posts.length > 0">
+    <h3>Список постов</h3>
+    <post-item
+      :post="post"
+      v-for="post in posts"
+      :key="post.id"
+      @remove="$emit('remove', post)"
+    ></post-item>
+  </div>
+  <h2 v-else style="color: red">Список постов пуст</h2>
+</template>
+
+<script>
+import PostItem from './PostItem.vue'
+export default {
+  components: { PostItem },
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped></style>
